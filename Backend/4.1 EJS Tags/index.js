e29d6df76d3d3b9cb789ef7c27d937d0423780e0
Burak Maden <burak.maden@windowslive.com>
2024-05-31 19:12:14 +0300

    added ejs tags example

---
+++ Backend/4.1 EJS Tags/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+const port = 3500;
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<strong>This is some strong text</strong>",
+  };
+  res.render("index.ejs", data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
